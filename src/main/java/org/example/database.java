5ca253861c8cb761cb1bb996fbc14aa4989c88f6
bhuvaneshwari-k <123533635+bhuvaneshwari-k@users.noreply.github.com>
2023-02-27 11:46:37 +0530

    Update database.java

--- src/main/java/org/example/database.java
+++ src/main/java/org/example/database.java
@@ -3,7 +3,7 @@ import java.sql.*;
 import java.util.*;
 import java.util.logging.Logger;
 public class Database {
-    static Logger l=Logger.getLogger("com.api.jar");
+    static Logger l=Logger.getLogger("welcome");
     static Database db;
     Connection conn;
     private  Database(){}
@@ -22,7 +22,7 @@ public class Database {
     }
     public static void main(String[] args) throws SQLException {
         Scanner s=new Scanner(System.in);
-        static Logger l=Logger.getLogger("com.api.jar");
+        static Logger l=Logger.getLogger("welcome");
         l.info("Enter the url:");
     String url=s.nextLine();
         l.info("Enter the username:");
